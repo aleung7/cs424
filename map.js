76050f767b037494046cbe91854bc5db41d83a9a
DWang5090 <davidcxwang@hotmail.com>
2014-03-11 10:01:46 -1000

    New Revisions

--- map.js
+++ map.js
@@ -1,56 +1,78 @@
-var map_svg = d3.select("body")
+var state_svg = d3.select("body")
 				.append("svg")
-				.attr("id", "map")
+				.attr("id", "states")
 				
-var map_width = 1280,
-	map_height = 720;
+var map_width = 580,
+	map_height = 320;
 
 
-var projection = d3.geo.albersUsa()
-					.translate([300, 400])
-					.scale([800]);
-
-var path = d3.geo.path()
-	.projection(projection);
-
-map_svg.attr("width", map_width)
+state_svg.attr("width", map_width)
 		.attr("height", map_height);
+
+d3.csv("/data_csv/country_export.csv", function(data){
+	console.log(data)
+	d3.json("us.json",function(us){
+			var projection = d3.geo.albersUsa()
+								.translate([300, 150])
+								.scale([650]);
+
+			var path = d3.geo.path()
+				.projection(projection);
+			
 		
-d3.json("us.json",function(json){
-	console.log(json);
-	
-	map_svg.selectAll("path.states")
-			.data(json.features)
-			.enter()
-			.append("path")
-			.attr("class", "states")
-			.attr("d",path)
-			.attr("fill", "#777")
-			.attr("stroke", "#CCC")
-				
+			state_svg.selectAll("path.states")
+					.data(us.features)
+					.enter()
+					.append("path")
+					.attr("class", function(d){return "state " + d.properties.name.toLowerCase();})
+					.attr("d",path)
+					.attr("fill", "#777")
+					.attr("stroke", "#CCC")
+					.on("mouseover", function(d){
+						d3.select(this).transition().duration(200).attr("fill", "red")
+						for(var i = 0; i < data.length; i++){
+							if(d.properties.name.toLowerCase() === data[i]["statename"].toLowerCase()){
+								if(data[i]["countryd"] !== "World" && data[i]["countryd"] !== "Top 25"){
+									console.log(data[i]["countryd"])
+									d3.select("path.country." + data[i]["countryd"].toLowerCase()).transition().duration(100).attr("fill","red")
+								}
+							}
+						}
+					})
+					.on("mouseout", function(d){
+						d3.select(this).transition().duration(200).attr("fill", "#777")
+						for(var i = 0; i < data.length; i++){
+							if(d.properties.name.toLowerCase() === data[i]["statename"].toLowerCase()){
+								if(data[i]["countryd"] !== "World" && data[i]["countryd"] !== "Top 25"){
+									console.log(data[i]["countryd"])
+									d3.select("path.country." + data[i]["countryd"].toLowerCase()).transition().duration(100).attr("fill","#777")
+								}
+							}
+						}
+					})
+	});
 });
 
+var country_svg = d3.select("body").append("svg").attr("id", "countries")
 
-d3.csv("alabama.csv", function(data){
-	console.log(data);
+country_svg.attr("width", 800)
+				.attr("height", 800)
+				
+d3.json("world-countries.json", function(country){
+	var projection = d3.geo.equirectangular()
+						.translate([400, 550])
+						.scale(120);
 
-})
+	var path = d3.geo.path()
+					.projection(projection)
 
-// d3.json("world-countries.json", function(json){
-// 	
-// 	var projection = d3.geo.azimuthalEqualArea()
-// 						.translate([400, 500])
-// 						.scale(100);
-// 						
-// 	var path = d3.geo.path()
-// 					.projection(projection)
-// 	
-// 	console.log(json)
-// 	map_svg.selectAll("path.countries")
-// 		.data(json.features)
-// 		.enter()
-// 		.append("path")
-// 		.attr("class", "countries")
-// 		.attr("d",path)
-// 		.attr("fill", "#777")
-// 		.attr("stroke", "#CCC")	
+	country_svg.selectAll("path.countries")
+		.data(country.features)
+		.enter()
+		.append("path")
+		.attr("class",function(d){return "country " + d.properties.name.toLowerCase();})
+		.attr("d",path)
+		.attr("fill", "#777")
+		.attr("stroke", "#CCC")	
+	
+});
